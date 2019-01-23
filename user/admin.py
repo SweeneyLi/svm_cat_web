6c7d0f95f223895d0bbc3924f620e91bdb6a6a79
sweeney <sweeney@sweeneys-MacBook-Pro.local>
2019-01-23 08:33:45 +0800

    use the user

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+admin.site.unregister(User)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['username', 'password', 'email']}),
+        ('Important dates', {'fields': ['last_login', 'date_joined'], 'classes': ['collapse']}),
+    ]
+
+    # list_display = ('question_text', 'pub_date', 'was_published_recently')
+    # list_filter = ['pub_date']
+    # search_fields = ['question_text']
+
