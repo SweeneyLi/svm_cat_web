647c130059ac1b0ee85cacb34b0f0c8368ce9812
SweeneyLi <sweeneylee@qq.com>
2019-01-24 20:29:13 +0800

    rectify the name of image

--- picture/models.py
+++ picture/models.py
@@ -6,15 +6,19 @@ from user.models import UserProfile
 
 
 def user_directory_path(instance, filename):
+    if instance.pic_name.lower().split('.')[-1] in ['jpg', 'png', 'jpeg']:
+        name = instance.pic_name
+    else:
+        name = instance.pic_name + '.' + filename.split('.')[-1]
 
-    return os.path.join(str(instance.user_id), instance.category, filename)
+    return os.path.join(str(instance.user_id), instance.category, name)
 
 
 # Create your models here.
 class Picture(models.Model):
 
     user_id = models.IntegerField('user_id', default=1)
-    pic_name = models.CharField("图片名", max_length=100, blank=False, default=timezone.now())
+    pic_name = models.CharField("图片名", max_length=100, blank=False, default='default')
     category = models.CharField("类别", max_length=100, blank=False, default='default')
     image = models.ImageField("图片", upload_to=user_directory_path, blank=False)
     upload_date = models.DateField(default=date.today)
